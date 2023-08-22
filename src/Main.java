0ed8379fa723404e255660c42be7988701c77317
BelkaLu <levikina.ea@gmail.com>
2023-08-22 20:09:40 +0300

    new file

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Баланс счета");
+        int x = 300;
+        int balance = 1000;
+        int i = x + (balance);
+        if (x > 1000) {
+            System.out.println((x / 100) + i);
+            ;
+            System.out.println("Сумма зачисления c бонусом");
+        } else {
+            int z = x + i;
+            {
+                System.out.println(z = x + (balance));
+                System.out.print("Сумма зачисления без бонуса");
+            }
+
+
+        }
+    }
+
+}
+
+
+
+
+

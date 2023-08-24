3f052f9996687454be3a6712439c4f8edc0a4ed5
BelkaLu <levikina.ea@gmail.com>
2023-08-25 00:43:03 +0300

    up date name

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Баланс счета");
-        int x = 300;
+        int pay = 1300;
         int balance = 1000;
-        int i = x + (balance);
-        if (x > 1000) {
-            System.out.println((x / 100) + i);
+        int i = pay + (balance);
+        if (pay > 1000) {
+            System.out.println((pay / 100) + i);
             ;
             System.out.println("Сумма зачисления c бонусом");
         } else {
-            int z = x + i;
+            int z = pay + i;
             {
-                System.out.println(z = x + (balance));
+                System.out.println(z = pay + (balance));
                 System.out.print("Сумма зачисления без бонуса");
             }
 

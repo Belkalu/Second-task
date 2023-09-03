072c1c0d815706eb795a5abb17448f48052b2e42
BelkaLu <levikina.ea@gmail.com>
2023-09-03 17:35:38 +0300

    fixing variable names

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ public class Main {
         System.out.println("Баланс счета");
         int pay = 1300;
         int balance = 1000;
-        int i = pay + (balance);
+        int current = pay + (balance);
         if (pay > 1000) {
-            System.out.println((pay / 100) + i);
-            ;
+            System.out.println((pay / 100) + current);
             System.out.println("Сумма зачисления c бонусом");
         } else {
-            int z = pay + i;
+            int nobonus = pay + current;
             {
-                System.out.println(z = pay + (balance));
+                System.out.println(nobonus = pay + (balance));
                 System.out.print("Сумма зачисления без бонуса");
             }
 

3dd7d32d9ff3c744a4d9533debdf85cac52cc587
BelkaLu <levikina.ea@gmail.com>
2023-09-06 13:19:56 +0300

    balance line added

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Баланс счета");
-        int pay = 1300;
-        int balance = 1000;
-        int current = pay + (balance);
+        int pay = 1300; //платеж
+        int balance = 1000; //начальный баланс
+        int bonus; //бонус
+        int balansfinal; //итоговый баланс
+
         if (pay > 1000) {
-            System.out.println((pay / 100) + current);
-            System.out.println("Сумма зачисления c бонусом");
+            bonus = pay / 100;
+            balansfinal = balance + pay + bonus;
+            System.out.println("Сумма зачисления c бонусом:" + balansfinal);
+            System.out.println("Бонус зачислен на сумму:" + bonus);
         } else {
-            int nobonus = pay + current;
-            {
-                System.out.println(nobonus = pay + (balance));
-                System.out.print("Сумма зачисления без бонуса");
-            }
-
-
+            balansfinal = pay + balance;
+            System.out.print("Сумма зачисления без бонуса" + balansfinal);
         }
     }
-
 }
 
 
